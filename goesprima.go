378df85675bd19c4a21163198ebb14a2e8dcda28
frontexas <forfron@gmail.com>
2018-01-08 13:27:52 -0600

    Remove goesprima.go for now

--- goesprima.go
+++ goesprima.go
@@ -1,11 +1,3 @@
 package main
 
-var comment_handler_1 = nil /* require("./comment-handler"); */
-var parser_1 = nil /* require(./parser) */
-var tokenizer_1 = nil /* require(./tokenizer) */
-
-func (self *_goesprima) parse(code String, options interface{}, delegate interface{}) {
-	var commentHandler = nil; 
-
-}
 
