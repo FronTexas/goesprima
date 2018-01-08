a155fd1b858e208b8d73aac1b407a2690a3161c1
frontexas <forfron@gmail.com>
2018-01-08 14:19:42 -0600

    Added RawToken struct and some util function to scanner.go

--- scanner.go
+++ scanner.go
@@ -1,5 +1,18 @@
 package main 
 
+import(
+	"strings"
+	"regexp"
+)
+
+func hexValue(ch string) int {
+	return strings.Index("0123456789abcdef", strings.ToLower(ch))
+}
+
+func octalValue (ch string) int {
+	return strings.Index("01234567", ch)
+}
+
 type Position struct {
 	line int 
 	column int 
@@ -10,3 +23,30 @@ type SourceLocation struct {
 	end Position
 	source string
 }
+
+// TODO: Find a better way to differentiate scanner.Comment and comment-handler.Comment
+type Comment_scanner struct {
+	multiline bool
+	slice []int
+	_range []int
+	loc SourceLocation
+}
+
+type RawToken struct {
+	_type Token
+	value_string string
+	value_number int 
+	pattern string 
+	flags string
+	regex regexp.Regexp
+	octal bool
+	cooked string
+	head bool
+	tail bool
+	lineNumber int 
+	lineStart int 
+	start int 
+	end int
+}
+
+
---
+++ token.go
@@ -0,0 +1,17 @@
+package main 
+
+type Token int
+
+const (
+	_ Token = iota 
+	BooleanLiteral
+	EOF
+	Identifier
+	Keyword
+	NullLiteral
+	NumericLiteral
+	Punctuator
+	StringLiteral
+	RegularExpression
+	Template
+)
